797baa1e2a16ca689fe794f2bc8f88ff136f922a
Itziks <mail.itzik@gmail.com>
2020-09-03 09:10:40 +0300

    updating doc

--- src/main/java/com/netomedia/exam/hangman/player/HangmanPlayer.java
+++ src/main/java/com/netomedia/exam/hangman/player/HangmanPlayer.java
@@ -6,9 +6,11 @@ public class HangmanPlayer {
 
     private static HangmanServer server = new HangmanServer();
 
+    /**
+     * This is the entry point of your Hangman Player
+     * To start a new game call server.startNewGame()
+     */
     public static void main(String[] args) throws Exception {
-        //Implement your Hangman Player here
 
-        //server.startNewGame();
     }
 }
